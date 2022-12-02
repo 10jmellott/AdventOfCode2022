f2ae22b6d1431740d2a51a7c0d0b7a4817a16a20
Joshua Mellott-Lillie <10jmellott@gmail.com>
2022-12-01 19:27:28 -0500

    Day 1: Calorie Counting

--- src/day01.py
+++ src/day01.py
@@ -1,14 +1,29 @@
+from functools import reduce
+
 def _load_file():
 	# Open the file in read-only mode
 	return open('data/01.txt', 'r')
 
-def _part1():
+def _getRations():
 	f = _load_file()
-	return None
+	elves = list()
+	currentElf = list()
+	for line in f:
+		if line == '\n':
+			elves.append(currentElf)
+			currentElf = list()
+		else:
+			currentElf.append(int(line.strip('\n')))
+	elves.append(currentElf)
+	calories = list(map(lambda l: reduce(lambda a, b: a+b, l), elves))
+	calories.sort()
+	return calories
+
+def _part1():
+	return _getRations()[-1]
 
 def _part2():
-	f = _load_file()
-	return None
+	return sum(_getRations()[-3:])
 
 def main():
 	return _part1(), _part2()
